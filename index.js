03cc412457b573c1b557d88c2046cd1195fcdd7a
Jarno Le Conté <jarno@pti.nl>
2019-02-24 22:07:00 +0100

    Add default proto options

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const path = require('path');
 
+const protoOptions = {
+  keepCase: false,
+  longs: String,
+  enums: Number,
+  defaults: false,
+  oneofs: false,
+  arrays: true,
+};
+
 function getProtoPath(...paths) {
   return path.join(__dirname, 'protos', ...paths);
 }
 
+
 module.exports = {
+    protoOptions,
     getProtoPath,
 };
